551948694f641334e71a8d840741c3450426e8a0
EmilDok <kyrbicanal2@gmail.com>
2022-05-01 16:09:15 +0300

    add folders

---
+++ ObserverSubscribers/ObserverSubscribers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ObserverSubscribers
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            var Publisher = new Publisher();
+            var client1 = new Subscriber1(3000, "Никита");
+            Publisher.Attach(client1);
+
+            var client2 = new Subscriber2(1700, "Андрей");
+            Publisher.Attach(client2);
+
+            List<string> newProducts = new List<string>();
+
+            newProducts.Add("Кофе Bushido");
+
+            Publisher.AddSomeProducts(newProducts);
+
+            newProducts.Add("Сырок Б.Ю. Александров");
+            Publisher.AddSomeProducts(newProducts);
+        }
+    }
+}
